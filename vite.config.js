9caad7da2af4568be448b5da8a04ed9b95172967
Jumarf123 <simonenkob819@gmail.com>
2025-11-14 14:10:23 +0800

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ezbio/', // Для GitHub Pages: замените на имя вашего репозитория
+  base: '/checkbio123/', // Имя вашего репозитория
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
